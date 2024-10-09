cda5e0a4d25b4858da4d0bdda0382ea7b6cdfa18
Natallia Mashkarova <113904743+6Ash6@users.noreply.github.com>
2024-10-09 17:22:38 +0300

    3 task

--- Main.java
+++ Main.java
@@ -57,12 +57,52 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Ошибка: " + e.getClass());
         }
+
+
+        //3 task
+        ArrayService_3task arrayService = new ArrayService_3task();
+        ArrayFileReader_3task reader_3task = new ArrayFileReader_3task();
+
+        int[] array_3task = {1, -2, 3, -4, 5};
+
+        // Пример работы с ArrayService
+        System.out.println("Минимум: " + arrayService.findMin(array_3task).orElse(-1));
+        System.out.println("Максимум: " + arrayService.findMax(array_3task).orElse(-1));
+        System.out.println("Среднее: " + arrayService.average(array_3task));
+        System.out.println("Сумма: " + arrayService.sum(array_3task));
+        System.out.println("Количество положительных: " + arrayService.countPositive(array_3task));
+        System.out.println("Количество отрицательных: " + arrayService.countNegative(array_3task));
+
+        // Пример сортировки массива и чтения данных из файла
+        try {
+            int[] arrayFromFile = reader_3task.readArrayFromFile("D:\\Java\\Lab1\\src\\input.txt");
+            System.out.println("Исходный массив:");
+            printArray(arrayFromFile);
+
+            // Сортировка
+            int[] sortedArray = reader_3task.sortArray(arrayFromFile);
+            System.out.println("Отсортированный массив:");
+            printArray(sortedArray);
+
+            // Обратная сортировка
+            int[] reverseSortedArray = reader_3task.reverseSortArray(arrayFromFile);
+            System.out.println("Массив, отсортированный в обратном порядке:");
+            printArray(reverseSortedArray);
+
+            // Пузырьковая сортировка
+            int[] bubbleSortedArray = reader_3task.bubbleSort(arrayFromFile);
+            System.out.println("Массив после пузырьковой сортировки:");
+            printArray(bubbleSortedArray);
+
+        } catch (IOException e) {
+            System.err.println("Ошибка чтения файла: " + e.getMessage());
+        }
     }
-        private static void printArray(int[] array) {
+
+    private static void printArray(int[] array) {
         for (int value : array) {
             System.out.print(value + " ");
         }
         System.out.println();
     }
-
 }
